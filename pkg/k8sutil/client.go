0f3272443242562296997ada382874f4f221a493
Höhl, Lukas <lukas.hoehl@accso.de>
2021-11-09 15:20:31 +0100

    init

---
+++ pkg/k8sutil/client.go
@@ -0,0 +1,23 @@
+package k8sutil
+
+import (
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"github.com/hown3d/chat-operator/pkg/client/internalclientset"
+)
+
+//NewClientSet creates a new kubernetes rest config
+func NewClientSet(kubeconfig *string) (*kubernetes.Clientset, error) {
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		return nil, err
+	}
+	// create the clientset
+	clientset := kubernetes.NewForConfigOrDie(config)
+	return clientset, nil
+}
+
+func NewChatClientSet(kubeconfig *string) {
+}
